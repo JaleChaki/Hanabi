31e56ba77a0983f25fdbc72e17c9ee40ff8ddb45
Станислав Клесарев <klesarevklesarevstas@gmail.com>
2022-08-19 23:23:33 +0400

    first attempt to use socket

--- Hanabi/ClientApp/src/components/Home.js
+++ Hanabi/ClientApp/src/components/Home.js
@@ -1,26 +1,85 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import * as signalR from "@microsoft/signalr";
 
 export class Home extends Component {
-  static displayName = Home.name;
+    static displayName = Home.name;
 
-  render () {
-    return (
-      <div>
-        <h1>Hello, world!</h1>
-        <p>Welcome to your new single-page application, built with:</p>
-        <ul>
-          <li><a href='https://get.asp.net/'>ASP.NET Core</a> and <a href='https://msdn.microsoft.com/en-us/library/67ef8sbd.aspx'>C#</a> for cross-platform server-side code</li>
-          <li><a href='https://facebook.github.io/react/'>React</a> for client-side code</li>
-          <li><a href='http://getbootstrap.com/'>Bootstrap</a> for layout and styling</li>
-        </ul>
-        <p>To help you get started, we have also set up:</p>
-        <ul>
-          <li><strong>Client-side navigation</strong>. For example, click <em>Counter</em> then <em>Back</em> to return here.</li>
-          <li><strong>Development server integration</strong>. In development mode, the development server from <code>create-react-app</code> runs in the background automatically, so your client-side resources are dynamically built on demand and the page refreshes when you modify any file.</li>
-          <li><strong>Efficient production builds</strong>. In production mode, development-time features are disabled, and your <code>dotnet publish</code> configuration produces minified, efficiently bundled JavaScript files.</li>
-        </ul>
-        <p>The <code>ClientApp</code> subdirectory is a standard React application based on the <code>create-react-app</code> template. If you open a command prompt in that directory, you can run <code>npm</code> commands such as <code>npm test</code> or <code>npm install</code>.</p>
-      </div>
-    );
-  }
+    constructor() {
+        super();
+    }
+    
+    componentDidMount() {
+        this.initiateHubConnection();
+    }
+
+    initiateHubConnection() {
+        let connection = new signalR.HubConnectionBuilder()
+            .configureLogging(signalR.LogLevel.Debug)
+            .withUrl("/testhub", {
+                skipNegotiation: true,
+                transport: signalR.HttpTransportType.WebSockets
+            })
+            .build();
+
+        //Disable the send button until connection is established.
+        document.getElementById("sendButton").disabled = true;
+
+        connection.on("ReceiveMessage", function (user, message) {
+            var li = document.createElement("li");
+            document.getElementById("messagesList").appendChild(li);
+            // We can assign user-supplied strings to an element's textContent because it
+            // is not interpreted as markup. If you're assigning in any other way, you 
+            // should be aware of possible script injection concerns.
+            li.textContent = `${user} says ${message}`;
+        });
+
+        connection.start().then(function () {
+            document.getElementById("sendButton").disabled = false;
+        }).catch(function (err) {
+            return console.error(err.toString());
+        });
+
+        document.getElementById("sendButton").addEventListener("click", function (event) {
+            var user = document.getElementById("userInput").value;
+            var message = document.getElementById("messageInput").value;
+            connection.invoke("SendMessage", user, message).catch(function (err) {
+                return console.error(err.toString());
+            });
+            event.preventDefault();
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <div className="container">
+                    <div className="row">&nbsp;</div>
+                    <div className="row">
+                        <div className="col-2">User</div>
+                        <div className="col-4"><input type="text" id="userInput"/></div>
+                    </div>
+                    <div className="row">
+                        <div className="col-2">Message</div>
+                        <div className="col-4"><input type="text" id="messageInput"/></div>
+                    </div>
+                    <div className="row">&nbsp;</div>
+                    <div className="row">
+                        <div className="col-6">
+                            <input type="button" id="sendButton" value="Send Message"/>
+                        </div>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-12">
+                        <hr/>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-6">
+                        <ul id="messagesList"></ul>
+                    </div>
+                </div>
+            </div>
+        );
+    }
 }
