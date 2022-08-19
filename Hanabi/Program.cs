54b523f75abc1ae15701ffdf545ebfe5a10e3a63
JaleChaki <github_c923@jalechaki.com>
2022-08-19 20:57:00 +0400

    create react app template

---
+++ Hanabi/Program.cs
@@ -0,0 +1,12 @@
+namespace Hanabi {
+    internal static class Program {
+        public static void Main(string[] args) {
+            var builder = CreateHostBuilder(args);
+            builder.Build().Run();
+        }
+
+        private static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder => webBuilder.UseStartup<Startup>());
+    }
+}
---
+++ Hanabi/Startup.cs
@@ -0,0 +1,23 @@
+﻿namespace Hanabi {
+    public class Startup {
+
+        public void ConfigureServices(IServiceCollection services) {
+            services.AddControllers();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment environment) {
+            if(!environment.IsDevelopment()) {
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints => {
+                endpoints.MapControllers();
+                endpoints.MapFallbackToFile("index.html");
+            });
+        }
+    }
+}
