405a6e666383ff509e45e2c7837ed5fcdd406f90
JaleChaki <github_c923@jalechaki.com>
2022-08-20 17:32:36 +0400

    add hub endpoints

--- Hanabi/Hubs/GameHub.cs
+++ Hanabi/Hubs/GameHub.cs
@@ -1,4 +1,5 @@
-﻿using Hanabi.Services;
+﻿using Hanabi.Models;
+using Hanabi.Services;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.SignalR;
@@ -17,6 +18,32 @@ namespace Hanabi.Hubs {
         public async Task GetGameState() {
             var userId = GetUserGuid(Context.User.Identity.Name);
             await Clients.Caller.SendAsync("SetGameState", GameService.GetGameState(userId));
+        }
+
+        public Task MakeColorHint(string targetPlayerNickname, int color) {
+            var userId = GetUserGuid(Context.User.Identity.Name);
+            var targetPlayerId = GetUserGuid(targetPlayerNickname);
+            GameService.GetController(userId).MakeHint(userId, targetPlayerId, HintOptions.FromCardColor(color));
+            return Task.CompletedTask;
+        }
+
+        public Task MakeNumberHint(string targetPlayerNickname, int number) {
+            var userId = GetUserGuid(Context.User.Identity.Name);
+            var targetPlayerId = GetUserGuid(targetPlayerNickname);
+            GameService.GetController(userId).MakeHint(userId, targetPlayerId, HintOptions.FromCardNumber(number));
+            return Task.CompletedTask;
+        }
+
+        public Task DropCard(int cardIndex) {
+            var userId = GetUserGuid(Context.User.Identity.Name);
+            GameService.GetController(userId).DropCard(userId, cardIndex);
+            return Task.CompletedTask;
+        }
+
+        public Task PlayCard(int cardIndex) {
+            var userId = GetUserGuid(Context.User.Identity.Name);
+            GameService.GetController(userId).PlayCard(userId, cardIndex);
+            return Task.CompletedTask;
         }
 
         private Guid GetUserGuid(string nickname) {
