3c11b1e9d84ff00203fd55c8c4c10e27baaaaccf
Staziz <klesarevklesarevstas@gmail.com>
2025-02-25 23:10:41 +0400

    Patch text a bit

--- Hanabi/ClientApp/src/components/MainField/GameEndPanel.tsx
+++ Hanabi/ClientApp/src/components/MainField/GameEndPanel.tsx
@@ -14,10 +14,10 @@ export const GameEndPanel: FC<GameEndPanelProps> = ({ status, totalScore }) => {
                 : null
             }
             {status === GameStatus.Victory
-                ? <h3>You final score is {totalScore}. Try harder next time, and maybe you'll do it even better :)</h3>
+                ? <h3>Your final score is {totalScore}. Try harder next time, and maybe you'll do it even better :)</h3>
                 : null
             }
-            <h4>Good luck!</h4>
+            <h4>Good luck next time!</h4>
         </div>
     );
 }
