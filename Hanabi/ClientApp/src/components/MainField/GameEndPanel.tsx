d62aa0f60923af3a1172cef1ed83e2e75d92c0ce
Staziz <klesarevklesarevstas@gmail.com>
2025-02-25 22:38:37 +0400

    Potentially, it will display success/failure status

---
+++ Hanabi/ClientApp/src/components/MainField/GameEndPanel.tsx
@@ -0,0 +1,23 @@
+import React, { FC } from "react";
+import { GameStatus } from "../../SerializationTypes/GameStatus";
+
+type GameEndPanelProps {
+    status: GameStatus
+    totalScore: number
+}
+export const GameEndPanel: FC<GameEndPanelProps> = ({ status, totalScore }) => {
+    return(
+        <div className="game-end-panel">
+            <h1>You {status === GameStatus.FlawlessVictory ? "dramatically" : ""} {status === GameStatus.Failure ? "failed!" : "won the game!"}</h1>
+            {status !== GameStatus.Failure 
+                ? <h2>Congratulations on {status === GameStatus.FlawlessVictory ? "such a tremendous" : ""} success!</h2>
+                : null
+            }
+            {status === GameStatus.Victory
+                ? <h3>You final score is {totalScore}. Try harde next time, and maybe you'll do it even better :)</h3>
+                : null
+            }
+            <h4>Good luck!</h4>
+        </div>
+    );
+}
--- Hanabi/ClientApp/src/components/MainField/MainLayout.tsx
+++ Hanabi/ClientApp/src/components/MainField/MainLayout.tsx
@@ -9,27 +9,34 @@ import { IPlayerActions } from "../Players/Player";
 import { IGameState } from "../../SerializationTypes/IGameState";
 
 import "./MainLayout.scss"
+import { GameStatus } from "../../SerializationTypes/GameStatus";
+import { GameEndPanel } from "./GameEndPanel";
 
 type MainLayoutProps = {
     gameState: IGameState,
     playerActions: IPlayerActions
 }
-export const MainLayout: FC<MainLayoutProps> = ({ gameState: { turnIndex, players, informationTokens, fuseTokens, fireworks }, playerActions }) => {
+export const MainLayout: FC<MainLayoutProps> = ({ gameState: { turnIndex, players, informationTokens, fuseTokens, fireworks, gameStatus }, playerActions }) => {
 
     return (
         <div className="main-field">
             <History></History>
-            <div className="main-wrapper">
-                <Players turnIndex={turnIndex} players={players} actions={playerActions} ></Players>
-                <div className="main-footer">
-                    <div className="card-deck"></div>
-                    <Solitaire fireworks={fireworks}></Solitaire>
-                    <div className="token-storages">
-                        <TokenStorage type={TokenType.Info} currentCount={informationTokens}></TokenStorage>
-                        <TokenStorage type={TokenType.Fuse} currentCount={fuseTokens}></TokenStorage>
+            {gameStatus === GameStatus.InProgress
+                ? <div className="main-wrapper">
+                    <Players turnIndex={turnIndex} players={players} actions={playerActions} ></Players>
+                    <div className="main-footer">
+                        <div className="card-deck"></div>
+                        <Solitaire fireworks={fireworks}></Solitaire>
+                        <div className="token-storages">
+                            <TokenStorage type={TokenType.Info} currentCount={informationTokens}></TokenStorage>
+                            <TokenStorage type={TokenType.Fuse} currentCount={fuseTokens}></TokenStorage>
+                        </div>
                     </div>
                 </div>
-            </div>
+                : <div className="main-wrapper">
+                    <GameEndPanel status={gameStatus} totalScore={fireworks.reduce((acc, cur) => acc + cur, 0)}/>
+                </div>
+            }
         </div>
     )
 }
