4203d316cd02969be1e881a68ee3890604bee7e7
Staziz <klesarevklesarevstas@gmail.com>
2025-03-13 22:48:36 +0400

    Remove NavMenu

--- Hanabi/ClientApp/src/components/Layout.tsx
+++ Hanabi/ClientApp/src/components/Layout.tsx
@@ -1,15 +1,10 @@
 import React, {FC, ReactNode} from 'react';
-import {Container} from 'reactstrap';
-import {NavMenu} from './NavMenu';
 
 type MyProps = { children?: Array<JSX.Element | ReactNode | null> | any }
 export const Layout: FC<MyProps> = (props) => {
     return (
         <div>
-            <NavMenu/>
-            <div>
-                {props.children}
-            </div>
+            {props.children}
         </div>
     );
 }
