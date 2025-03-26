a76f3d0252d8b356ddff8f75df7babe1e87e8c10
Staziz <klesarevklesarevstas@gmail.com>
2025-03-27 01:41:25 +0400

    Extract GameField component

---
+++ Hanabi/ClientApp/src/components/MainField/GameField.tsx
@@ -0,0 +1,49 @@
+import "./GameField.scss";
+import { Drawer } from "../Auxiliary/Drawer";
+import React, { FC, Fragment, ReactNode, useEffect, useState } from "react";
+import { History } from "./History";
+import { Players } from "../Players/Players";
+import { Deck } from "../Deck";
+import { Solitaire } from "./Solitaire";
+import { TokenStorage } from "../Tokens/TokenStorage";
+import { IGameState } from "../../SerializationTypes/IGameState";
+import { IPlayerActions } from "../Players/Player";
+import { TokenType } from "../Tokens/Token";
+
+type GameFieldProps = {
+    gameState: IGameState,
+    playerActions: IPlayerActions,
+}
+
+export const GameField: FC<GameFieldProps> = ({ 
+        gameState: { turnIndex, players, discardPile, informationTokens, fuseTokens, fireworks, cardsInDeck },
+        playerActions
+    }) => {
+    const [screenWidth, setScreenWidth] = React.useState(window.innerWidth);
+    React.useEffect(() => {
+        const handleResize = () => setScreenWidth(window.innerWidth);
+        window.addEventListener('resize', handleResize);
+        return () => window.removeEventListener('resize', handleResize);
+    }, []);
+    const isMobile = screenWidth < 768;
+    const [isHistoryShown, setIsHistoryShown] = React.useState(!isMobile);
+
+    return (
+        <Fragment>
+            <Drawer isOpen={isHistoryShown} onClose={() => setIsHistoryShown(false)}>
+                <History discardPile={discardPile}/>
+            </Drawer>
+            <div className="main-wrapper">
+                <Players turnIndex={turnIndex} players={players} actions={playerActions} ></Players>
+                <div className="main-footer">
+                    <Deck name="Deck:" cardsInDeck={cardsInDeck}/>
+                    <Solitaire fireworks={fireworks}></Solitaire>
+                    <div className="token-storages">
+                        <TokenStorage type={TokenType.Info} currentCount={informationTokens}></TokenStorage>
+                        <TokenStorage type={TokenType.Fuse} currentCount={fuseTokens}></TokenStorage>
+                    </div>
+                </div>
+            </div>
+        </Fragment>
+    );
+}
--- Hanabi/ClientApp/src/components/MainField/MainLayout.tsx
+++ Hanabi/ClientApp/src/components/MainField/MainLayout.tsx
@@ -1,51 +1,28 @@
 ﻿import React, { FC } from "react";
-import { History } from "./History";
-import { Players } from "../Players/Players";
-import { Solitaire } from "./Solitaire";
-import { TokenStorage } from "../Tokens/TokenStorage";
-import { TokenType } from "../Tokens/Token";
-import { IPlayer } from "../../SerializationTypes/IPlayer";
 import { IPlayerActions } from "../Players/Player";
 import { IGameState } from "../../SerializationTypes/IGameState";
 
 import "./MainLayout.scss"
 import { GameStatus } from "../../SerializationTypes/GameStatus";
 import { GameEndPanel } from "./GameEndPanel";
-import { HeldCard } from "../Card/HeldCard";
-import { Deck } from "../Deck";
 import { Lobby } from "./Lobby";
+import { GameField } from "./GameField";
 
 type MainLayoutProps = {
     gameState: IGameState,
     playerActions: IPlayerActions,
     startGame: () => {}
 }
-export const MainLayout: FC<MainLayoutProps> = ({ 
-        gameState: { turnIndex, players, discardPile, informationTokens, fuseTokens, fireworks, gameStatus, cardsInDeck, gameLink }, 
-        playerActions,
-        startGame 
-    }) => {
+export const MainLayout: FC<MainLayoutProps> = ({ gameState, playerActions, startGame }) => {
+    const { gameStatus, players, gameLink, fireworks } = gameState;
 
     return (
         <div className="main-field">
-            {gameStatus === GameStatus.Pending ?
-                <Lobby players={players} gameLink={gameLink} startGame={startGame}/>
+            {gameStatus === GameStatus.Pending 
+                ? <Lobby players={players} gameLink={gameLink} startGame={startGame}/>
                 : gameStatus === GameStatus.InProgress
-                ? <div className="main-wrapper">
-                    <History discardPile={discardPile}/>
-                    <Players turnIndex={turnIndex} players={players} actions={playerActions} ></Players>
-                    <div className="main-footer">
-                        <Deck name="Deck:" cardsInDeck={cardsInDeck}/>
-                        <Solitaire fireworks={fireworks}></Solitaire>
-                        <div className="token-storages">
-                            <TokenStorage type={TokenType.Info} currentCount={informationTokens}></TokenStorage>
-                            <TokenStorage type={TokenType.Fuse} currentCount={fuseTokens}></TokenStorage>
-                        </div>
-                    </div>
-                </div>
-                : <div className="main-wrapper">
-                    <GameEndPanel status={gameStatus} totalScore={fireworks.reduce((acc, cur) => acc + cur, 0)}/>
-                </div>
+                    ? <GameField gameState={gameState} playerActions={playerActions}/>
+                    : <GameEndPanel status={gameStatus} totalScore={fireworks.reduce((acc, cur) => acc + cur, 0)}/>
             }
         </div>
     )
