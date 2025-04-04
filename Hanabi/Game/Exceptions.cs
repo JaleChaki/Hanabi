5df253ca87bf60fada3a79e852966b695b1bb7a3
Staziz <klesarevklesarevstas@gmail.com>
2025-04-05 01:29:19 +0400

    Reorganize exceptions

--- Hanabi/Game/Exceptions.cs
+++ Hanabi/Game/Exceptions.cs
@@ -5,61 +5,69 @@ namespace Hanabi.Exceptions;
 // TODO: Localize all exceptions
 
 #region GameRelatexdExceptions
-public class GameNotFoundException : Exception {
+public class GameExceptionBase : Exception {
+    public GameExceptionBase(string message) : base(message) { }
+}
+public class GameNotFoundException : GameExceptionBase {
     public GameNotFoundException() : base("Game not found") { }
 }
-// TODO
-public class GameAlreadyStartedException : Exception {
-    public GameAlreadyStartedException(string message) : base(message) { }
+public class GameAlreadyStartedException : GameExceptionBase {
+    public GameAlreadyStartedException() : base("Game already started.") { }
 }
-public class GameAlreadyFullException : Exception {
+public class GameAlreadyFullException : GameExceptionBase {
     public GameAlreadyFullException() : base("Game is already at full capacity.") { }
 }
 // TODO
-public class GameNotStartedException : Exception {
+public class GameNotStartedException : GameExceptionBase {
     public GameNotStartedException(string message) : base(message) { }
 }
 // TODO
-public class GameNotInProgressException : Exception {
+public class GameNotInProgressException : GameExceptionBase {
     public GameNotInProgressException(string message) : base(message) { }
 }
-public class GameAlreadyFinishedException : Exception {
+public class GameAlreadyFinishedException : GameExceptionBase {
     public GameAlreadyFinishedException() : base("You cannot continue already finished game") { }
 }
-public class InvalidGameStateException : Exception {
+public class InvalidGameStateException : GameExceptionBase {
     public InvalidGameStateException(string message) : base(message) { }
 }
-public class InvalidGameActionException : Exception {
+public class InvalidGameActionException : GameExceptionBase {
     public InvalidGameActionException(string message) : base(message) { }
 }
 // TODO
-public class InvalidGameLinkException : Exception {
+public class InvalidGameLinkException : GameExceptionBase {
     public InvalidGameLinkException(string message) : base(message) { }
 }
 #endregion
 #region PlayerRelatedExceptions
-public class PlayerNotFoundException : Exception {
+public class PlayerExceptionBase : Exception {
+    public PlayerExceptionBase(string message) : base(message) { }
+}
+public class PlayerNotFoundException : PlayerExceptionBase {
     public PlayerNotFoundException(Guid playerId) : this(playerId.ToString()) { }
     public PlayerNotFoundException(string playerId) : base($"Player with GUID '{playerId}' not found") { }
     public PlayerNotFoundException(Guid playerId, string additionalMessage) : this(playerId.ToString(), additionalMessage) { }
     public PlayerNotFoundException(string playerId, string additionalMessage) : base($"Player with GUID '{playerId}' not found: {additionalMessage}") { }
 }
 // TODO
-public class PlayerAlreadyInGameException : Exception {
+public class PlayerAlreadyInGameException : PlayerExceptionBase {
     public PlayerAlreadyInGameException(string message) : base(message) { }
 }
-// public class InvalidPlayerStateException : Exception {
+// public class InvalidPlayerStateException : PlayerExceptionBase {
 //     public InvalidPlayerStateException(string message) : base(message) { }
 // }
 #endregion
 #region CardRelatedExceptions
-public class CardNotFoundException : Exception {
+public class CardExceptionBase : Exception {
+    public CardExceptionBase(string message) : base(message) { }
+}
+public class CardNotFoundException : CardExceptionBase {
     public CardNotFoundException(int cardIndex) : this(cardIndex.ToString()) { }
     public CardNotFoundException(CardColor cardColor) : this(cardColor.ToString()) { }
     public CardNotFoundException(string cardId) : base($"Invalid card {cardId}") { }
 }
 // TODO
-public class CardAlreadyHintedException : Exception {
+public class CardAlreadyHintedException : CardExceptionBase {
     public CardAlreadyHintedException(string message) : base(message) { }
 }
 #endregion
